aa97edb4c14cb5bf005e7da2dc00d2e392f60dcc
Jarl-Penguin <jarlpenguin@outlook.com>
2022-03-22 08:29:29 +0300

    [DNM] msm8937-common: Build tinymix
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I0a0f8a63e57e6bfbcf573503373e900a5921e9f1

--- msm8937.mk
+++ msm8937.mk
@@ -44,7 +44,8 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     libqcompostprocbundle \
     libqcomvisualizer \
-    libqcomvoiceprocessing
+    libqcomvoiceprocessing \
+    tinymix
 
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
